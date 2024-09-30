be39862f1d638395bb56b0d48dbff19cf47173b2
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-09-30 22:15:59 +0100

    build: Copy output file to GitHub runner (#103)
    This change streamlines the GitHub Actions workflow and enhances the
    output handling of the analyser:
    
    1. Removed unnecessary file checking steps from the `action-preview.yml`
    workflow file.
    2. Simplified the `run.sh` script by:
       - Removing debug echo statements and environment variable printing
       - Adding a step to check the output using `ls -a`
    - Implementing a step to copy the `repository_statistics.json` file to
    the GitHub workspace
    
    These modifications improve the efficiency of the workflow and ensure
    that the analysis results are properly stored in the expected location.
    
    fixes #104

--- run.sh
+++ run.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 set -e +x
 
-cd ..
-cd ..
-# Echo cwd
-echo "Current working directory: $(pwd)"
-# Show files
-ls -la
-
 printenv
+
+if [ "$CI" = "true" ]; then
+  # if running in GitHub Actions, change to the root of the repository
+  cd ..
+  cd ..
+fi
 
 # Run the analyser
 python -m analyser
+
+# Check the output
+ls -a
+
+if [ "$CI" = "true" ]; then
+  # if running in GitHub Actions, copy the output to the output directory
+  cp statistics/repository_statistics.json github/workspace/repository_statistics.json
+fi
