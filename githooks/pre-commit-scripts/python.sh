8e48442d2013086557fdbb22aa9154f1b1b8c5f3
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-10-11 11:52:36 +0100

    chore: Optimise pre-commit hook for Python files (#142)
    This change modifies the `python.sh` script in the
    `githooks/pre-commit-scripts` directory to conditionally run the `just
    install` and `just ruff-fix` commands. The script now checks if there
    are any changes in the `analyser/` folder before executing these
    commands. This optimisation ensures that the pre-commit hooks only run
    when necessary, potentially improving the commit process efficiency.
    
    fixes #141

--- githooks/pre-commit-scripts/python.sh
+++ githooks/pre-commit-scripts/python.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e +x
 
-just install
-just ruff-fix
+# Check if there are any changes in the watched folder
+if git diff --cached --name-only | grep -q "^analyser/"; then
+  just install
+  just ruff-fix
+fi
